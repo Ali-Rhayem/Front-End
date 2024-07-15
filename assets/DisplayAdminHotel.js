5e5da754001b8a8f05e01adb8050b75e5e04eedc
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-07-15 20:49:25 +0300

    adding delete hotels

---
+++ assets/DisplayAdminHotel.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function() {
+    async function fetchHotels() {
+        try {
+            const response = await axios.get('http://localhost/flight-full-stack/Back-End/Hotels/readAll.php');
+            const data = response.data;
+            const hotelsContainer = document.getElementById('hotels-container');
+            if (data.hotels) {
+                data.hotels.forEach(hotel => {
+                    const hotelDiv = document.createElement('div');
+                    hotelDiv.classList.add('hotel');
+
+                    let starsHtml = '';
+                    for (let i = 0; i < hotel.rate; i++) {
+                        starsHtml += '<i class="fa-solid fa-star filledstar"></i>';
+                    }
+                    for (let i = 0; i < 5 - hotel.rate; i++) {
+                        starsHtml += '<i class="fa-regular fa-star emptystar"></i>';
+                    }
+
+                    hotelDiv.innerHTML = `
+                        <div class="left">
+                            <div>
+                                <p class="hotelName">${hotel.name}</p>
+                                <p>${hotel.city}, ${hotel.country}, ${hotel.address}</p>
+                            </div>
+                            <p class="price">$${hotel.price_per_night}</p>
+                        </div>
+                        <div class="right">
+                            <i class="fa-solid fa-x delete" data-hotel-id="${hotel.hotel_id}"></i>
+                            <div>${starsHtml}</div>
+                            <div class="buttonContainer">
+                                <p>${hotel.available_rooms} rooms left</p>
+                                <button class="BookButton">
+                                    Edit <i class="fa-solid fa-arrow-right-long"></i>
+                                </button>
+                            </div>
+                        </div>
+                    `;
+                    
+                    
+                    const deleteIcon = hotelDiv.querySelector('.delete');
+                    
+                    deleteIcon.addEventListener('click', async function() {
+                        const hotelId = deleteIcon.getAttribute('data-hotel-id');
+                        // console.log(hotelId);
+                        try {
+                            const deleteResponse = await axios.post(
+                                'http://localhost/flight-full-stack/Back-End/Hotels/delete.php',
+                                {id: hotelId } 
+                            );
+                            hotelDiv.remove();
+                            // if (deleteResponse.data.success) {
+                            //     hotelDiv.remove();
+                            // } else {
+                            //     console.error('Failed to delete hotel:', deleteResponse.data.message);
+                            // }
+                        } catch (error) {
+                            console.error('Error deleting hotel:', error);
+                        }
+                    });
+
+                    hotelsContainer.appendChild(hotelDiv);
+                });
+            } else {
+                hotelsContainer.innerHTML = `<p>${data.message}</p>`;
+            }
+        } catch (error) {
+            console.error('Error fetching hotels:', error);
+        }
+    }
+
+    fetchHotels();
+});
