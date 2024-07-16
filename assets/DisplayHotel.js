5f80737f1c2e08b98478727a60ccdb0584a21451
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-07-16 20:03:08 +0300

    adding create hotel booking

--- assets/DisplayHotel.js
+++ assets/DisplayHotel.js
@@ -1,23 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    async function fetchHotels() {
-        try {
-            const response = await axios.get('http://localhost/flight-full-stack/Back-End/Hotels/readAll.php');
-            const data = response.data;
-            const hotelsContainer = document.getElementById('hotels-container');
-            if (data.hotels) {
-                data.hotels.forEach(hotel => {
-                    const hotelDiv = document.createElement('div');
-                    hotelDiv.classList.add('hotel');
+document.addEventListener("DOMContentLoaded", function () {
+  async function fetchHotels() {
+    try {
+      const response = await axios.get(
+        "http://localhost/flight-full-stack/Back-End/Hotels/readAll.php"
+      );
+      const data = response.data;
+      const hotelsContainer = document.getElementById("hotels-container");
+      if (data.hotels) {
+        data.hotels.forEach((hotel) => {
+          const hotelDiv = document.createElement("div");
+          hotelDiv.classList.add("hotel");
 
-                    let starsHtml = '';
-                    for (let i = 0; i < hotel.rate; i++) {
-                        starsHtml += '<i class="fa-solid fa-star filledstar"></i>';
-                    }
-                    for (let i = 0; i < 5 - hotel.rate; i++) {
-                        starsHtml += '<i class="fa-regular fa-star emptystar"></i>';
-                    }
+          let starsHtml = "";
+          for (let i = 0; i < hotel.rate; i++) {
+            starsHtml += '<i class="fa-solid fa-star filledstar"></i>';
+          }
+          for (let i = 0; i < 5 - hotel.rate; i++) {
+            starsHtml += '<i class="fa-regular fa-star emptystar"></i>';
+          }
 
-                    hotelDiv.innerHTML = `
+          hotelDiv.innerHTML = `
                     <div class="left">
                         <div>
                         <p class="hotelName">${hotel.name}</p>
@@ -29,21 +31,29 @@ document.addEventListener('DOMContentLoaded', function() {
                         <div>${starsHtml}</div>
                         <div class="buttonContainer">
                         <p>${hotel.available_rooms} rooms left</p>
-                        <button class="BookButton">
+                        <button class="BookButton" data-hotel-id="${hotel.hotel_id}">
                             Book Now <i class="fa-solid fa-arrow-right-long"></i>
-                            </button>
+                        </button>
                         </div>
                     </div>
                     `;
-                    hotelsContainer.appendChild(hotelDiv);
-                });
-            } else {
-                hotelsContainer.innerHTML = `<p>${data.message}</p>`;
-            }
-        } catch (error) {
-            console.error('Error fetching hotels:', error);
-        }
-    }
+          hotelsContainer.appendChild(hotelDiv);
+          const bookButton = hotelDiv.querySelector(".BookButton");
+        bookButton.addEventListener("click", function () {
+          const hotelId = bookButton.getAttribute("data-hotel-id");
+          window.location.href = `HotelBookings.html?id=${hotelId}`;
 
-    fetchHotels();
+        });
+        });
+
+        
+      } else {
+        hotelsContainer.innerHTML = `<p>${data.message}</p>`;
+      }
+    } catch (error) {
+      console.error("Error fetching hotels:", error);
+    }
+  }
+
+  fetchHotels();
 });
