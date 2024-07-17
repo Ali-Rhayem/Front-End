b44c34710d9bff836b47c292366b591725baf686
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-07-17 22:47:58 +0300

    adding search input for hotels, taxi, flights

--- scripts/DisplayFlight.js
+++ scripts/DisplayFlight.js
@@ -1,45 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let flights = [];
+
     async function fetchFlights() {
         try {
             const response = await axios.get('http://localhost/flight-full-stack/Back-End/flight/readAll.php');
             const data = response.data;
             const flightsContainer = document.getElementById('flights-container');
             if (data.flights) {
-                data.flights.forEach(flight => {
-                    const flightDiv = document.createElement('div');
-                    flightDiv.classList.add('flight');
-
-                    flightDiv.innerHTML = `
-                    <div class="left">
-                        <div>
-                            <p class="flightNumber">Flight Number: ${flight.flight_number}</p>
-                            <p>Departure: ${flight.departure_airport}</p>
-                            <p>Arrival: ${flight.arrival_airport}</p>
-                            <p>Departure Time: ${new Date(flight.departure_time).toLocaleString()}</p>
-                            <p>Arrival Time: ${new Date(flight.arrival_time).toLocaleString()}</p>
-                        </div>
-                        <p class="price">$${flight.price}</p>
-                    </div>
-                    <div class="right">
-                        <div class="buttonContainer">
-                            <p>${flight.available_seats} seats left</p>
-                            <button class="BookButton" data-flight-id="${flight.flight_id}">
-                                Book Now <i class="fa-solid fa-arrow-right-long"></i>
-                            </button>
-                        </div>
-                    </div>
-                    `;
-                    flightsContainer.appendChild(flightDiv);
-                });
-
-                const bookButtons = document.querySelectorAll('.BookButton');
-                bookButtons.forEach(button => {
-                    button.addEventListener('click', function () {
-                        const flightId = this.getAttribute('data-flight-id');
-                        sessionStorage.setItem('selectedFlightId', flightId);
-                        window.location.href = './ConfirmBookingFlight.html';
-                    });
-                });
+                flights = data.flights;
+                displayFlights(flights);
+                console.log('Fetched Flights:', flights); // Debugging
             } else {
                 flightsContainer.innerHTML = `<p>${data.message}</p>`;
             }
@@ -47,6 +17,60 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error fetching flights:', error);
         }
     }
+
+    function displayFlights(flightsToDisplay) {
+        const flightsContainer = document.getElementById('flights-container');
+        flightsContainer.innerHTML = '';
+        flightsToDisplay.forEach(flight => {
+            const flightDiv = document.createElement('div');
+            flightDiv.classList.add('flight');
+
+            flightDiv.innerHTML = `
+                <div class="left">
+                    <div>
+                        <p class="flightNumber">Flight Number: ${flight.flight_number}</p>
+                        <p>Departure: ${flight.departure_airport}</p>
+                        <p>Arrival: ${flight.arrival_airport}</p>
+                        <p>Departure Time: ${new Date(flight.departure_time).toLocaleString()}</p>
+                        <p>Arrival Time: ${new Date(flight.arrival_time).toLocaleString()}</p>
+                    </div>
+                    <p class="price">$${flight.price}</p>
+                </div>
+                <div class="right">
+                    <div class="buttonContainer">
+                        <p>${flight.available_seats} seats left</p>
+                        <button class="BookButton" data-flight-id="${flight.flight_id}">
+                            Book Now <i class="fa-solid fa-arrow-right-long"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+            flightsContainer.appendChild(flightDiv);
+        });
+
+        const bookButtons = document.querySelectorAll('.BookButton');
+        bookButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                const flightId = this.getAttribute('data-flight-id');
+                sessionStorage.setItem('selectedFlightId', flightId);
+                window.location.href = './ConfirmBookingFlight.html';
+            });
+        });
+    }
+
+    document.getElementById('searchInput').addEventListener('input', function() {
+        const query = this.value.toLowerCase();
+       
+        if (query.length > 1) {
+            const filteredFlights = flights.filter(flight =>
+                flight.flight_number.toLowerCase().includes(query)
+            );
+           
+            displayFlights(filteredFlights);
+        } else {
+            displayFlights(flights);
+        }
+    });
 
     fetchFlights();
 });
--- scripts/DisplayHotel.js
+++ scripts/DisplayHotel.js
@@ -1,58 +1,60 @@
-document.addEventListener('DOMContentLoaded', function() {
-    async function fetchHotels() {
-        try {
-            const response = await axios.get('http://localhost/flight-full-stack/Back-End/Hotels/readAll.php');
-            const data = response.data;
-            const hotelsContainer = document.getElementById('hotels-container');
-            if (data.hotels) {
-                data.hotels.forEach(hotel => {
-                    const hotelDiv = document.createElement('div');
-                    hotelDiv.classList.add('hotel');
+document.getElementById('searchInput').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    const hotelDivs = document.querySelectorAll('.hotel');
+    hotelDivs.forEach(div => {
+        const hotelName = div.querySelector('.hotelName').innerText.toLowerCase();
+        if (hotelName.includes(query)) {
+            div.style.display = 'flex';
+        } else {
+            div.style.display = 'none';
+        }
+    });
+});
 
-                    let starsHtml = '';
-                    for (let i = 0; i < hotel.rate; i++) {
-                        starsHtml += '<i class="fa-solid fa-star filledstar"></i>';
-                    }
-                    for (let i = 0; i < 5 - hotel.rate; i++) {
-                        starsHtml += '<i class="fa-regular fa-star emptystar"></i>';
-                    }
+async function fetchHotels() {
+    try {
+        const response = await axios.get('http://localhost/FlightManagementSystem/Hotels/readAll.php');
+        const data = response.data;
+        const hotelsContainer = document.getElementById('hotels-container');
+        if (data.hotels) {
+            data.hotels.forEach(hotel => {
+                const hotelDiv = document.createElement('div');
+                hotelDiv.classList.add('hotel');
 
-                    hotelDiv.innerHTML = `
-                    <div class="left">
-                        <div>
+                let starsHtml = '';
+                for (let i = 0; i < hotel.rate; i++) {
+                    starsHtml += '<i class="fa-solid fa-star filledstar"></i>';
+                }
+                for (let i = 0; i < 5 - hotel.rate; i++) {
+                    starsHtml += '<i class="fa-regular fa-star emptystar"></i>';
+                }
+
+                hotelDiv.innerHTML = `
+                <div class="left">
+                    <div>
                         <p class="hotelName">${hotel.name}</p>
                         <p>${hotel.city}, ${hotel.country}, ${hotel.address}</p>
-                        </div>
-                        <p class="price">$${hotel.price_per_night}</p>
                     </div>
-                    <div class="right">
-                        <div>${starsHtml}</div>
-                        <div class="buttonContainer">
+                    <p class="price">$${hotel.price_per_night}</p>
+                </div>
+                <div class="right">
+                    <div>${starsHtml}</div>
+                    <div class="buttonContainer">
                         <p>${hotel.available_rooms} rooms left</p>
-                        <button class="BookButton" data-hotel-id="${hotel.hotel_id}">
+                        <button class="BookButton">
                             Book Now <i class="fa-solid fa-arrow-right-long"></i>
-                            </button>
-                        </div>
+                        </button>
                     </div>
-                    `;
-                    hotelsContainer.appendChild(hotelDiv);
-                });
-
-                const bookButtons = document.querySelectorAll('.BookButton');
-                bookButtons.forEach(button => {
-                    button.addEventListener('click', function () {
-                        const hotelId = this.getAttribute('data-hotel-id');
-                        sessionStorage.setItem('selectedHotelId', hotelId);
-                        window.location.href = './ConfirmBookingHotel.html';
-                    });
-                });
-            } else {
-                hotelsContainer.innerHTML = `<p>${data.message}</p>`;
-            }
-        } catch (error) {
-            console.error('Error fetching hotels:', error);
+                </div>
+                `;
+                hotelsContainer.appendChild(hotelDiv);
+            });
+        } else {
+            hotelsContainer.innerHTML = `<p>${data.message}</p>`;
         }
+    } catch (error) {
+        console.error('Error fetching hotels:', error);
     }
+}
 
-    fetchHotels();
-});
+document.addEventListener('DOMContentLoaded', fetchHotels);
